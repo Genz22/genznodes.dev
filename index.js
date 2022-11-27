d578cd6f81e6bdc75c181bc53d4da9a0e7537579
genznodes <94878333+Genz22@users.noreply.github.com>
2022-11-27 23:21:18 +0700

    commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// REDIRECT SOSIAL MEDIA
+
+function redirMedium() {
+    window.location.href = "https://medium.com/@genzbit"
+}
+
+function redirMail() {
+    window.location.href = "mailto:lukivanz1@gmail.com"
+}
+
+
+// CONTENT
+
+// MUN -BLOCK CHAIN
+function redirMun() {
+    window.location.href = "https://blockexplorer.mun.money/validators/munvaloper1avzcjjewtv6af72wn9f2la5hxl885dmwwdvhh4" ;
+}
+
+
+// NIBIRU-CHAIN
+function redirNibid() {
+    window.location.href = "https://nibiru.explorers.guru/validator/nibivaloper12e0k5x3d36u5gj02h3t7nl6sggku009rxrtjtk";
+}
+
+// Stratos
+function redirStchaind() {
+    window.location.href = "https://explorer-tropos.thestratos.org/validators/stvaloper1j6zskl725na0udr8fwv30fnrfrly74g8a2pdqz";
+}
+
+// Inery
+function redirInery() {
+    window.location.href = "";
+}
+
+// Wormholes
+function redirWorm() {
+    window.location.href = "https://www.wormholesscan.com/#/AccountDetail/0x2950cC9F4Cb242627D3e35B7B2F316cDfFc2A4AE";
+}
+
+// OKP4
+function redirOkp() {
+    window.location.href = "";
+}
+
+// Zeeka
+function redirZeeka() {
+    window.location.href = "";
+}
+
+// loyal
+function redirLoyal() {
+    window.location.href = "https://ping-pub.joinloyal.io/loyal/staking/loyalvaloper1e429jm49fxgwzlryucl06e7tnjxrqq42kr36he";
+}
+
+// Aleph
+function redirAleph() {
+    window.location.href = "";
+}
+
+// Neutrond
+function redirNeutrond() {
+    window.location.href = "https://neutron.explorers.guru/validator/neutronvaloper1jrgm8m9s8tuujrmjk9g4d5c4n6uvkkxrv6erun";
+}
